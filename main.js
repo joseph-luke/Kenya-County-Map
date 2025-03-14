fe7dea27afeab87dbf239ab7221527cb557d7f1c
Joseph Nthumba <nthumba.joseph20@gmail.com>
2025-03-14 20:29:10 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true, // if you need Node integration
+      contextIsolation: false
+    }
+  });
+
+  // Load your main HTML file
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
